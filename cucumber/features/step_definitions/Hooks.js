ea5d7949d1d2d5df2462e0ee1044bdc9c2e4feb4
Justine Ballares <ballaresjustine@gmail.com>
2022-12-14 03:13:17 +0800

    detox init to BeforeAll hooks for faster tests

--- cucumber/features/step_definitions/Hooks.js
+++ cucumber/features/step_definitions/Hooks.js
@@ -1,14 +1,13 @@
-import { Before, After, BeforeAll } from '@cucumber/cucumber';
-import detox from 'detox';
+import { Before, AfterAll, BeforeAll } from '@cucumber/cucumber';
+import { init, cleanup, } from 'detox';
 
-// BeforeAll({ timeout: 120 * 1000 }, async () => {
-//   await detox.init("ios");
-// });
-Before({ timeout: 120 * 1000 }, async () => {
-  await detox.init("ios");
-  await device.launchApp();
+BeforeAll({ timeout: 60 * 1000 }, async () => {
+  await init("ios");
+  await device.launchApp({ newInstance: true });
+});
+Before(async () => {
   await device.reloadReactNative();
 });
-After({ timeout: 120 * 1000 }, async () => {
-  await detox.cleanup();
+AfterAll(async () => {
+  await cleanup();
 });
