e55fcfa9e55bab75b021f4d6c4728c925025d0b6
Justine Ballares <ballaresjustine@gmail.com>
2022-12-16 12:50:41 +0800

    added more utilities for testing

--- cucumber/features/helper/Utilities.js
+++ cucumber/features/helper/Utilities.js
@@ -1,6 +1,39 @@
 class Utilities {
-    async sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
+
+    async sleep(milliseconds) {
+        return new Promise(resolve => setTimeout(resolve, milliseconds));
+    }
+
+    async typeInElement(mobileElement, text) {
+        await mobileElement.replaceText(text);
+        await mobileElement.tapReturnKey();
+    }
+
+    async scrollToElement(targetElement, background, pixels, direction) {
+        if (direction === 'left' || direction === 'right') {
+            await this.scrollHorizontallyToElement(element(background), direction, targetElement);
+        } else {
+            await waitFor(targetElement)
+            .toBeVisible()
+            .whileElement(background)
+            .scroll(pixels, direction);
+        }
+    }
+
+    async scrollHorizontallyToElement(background, direction, targetElement) {
+        while(await this.softElementAssertion(targetElement) === false) {
+            const scrollDirection = direction === 'left' ? 'right' : 'left';
+            await background.swipe(scrollDirection, 'slow', 0.5);
+        }
+    }
+
+    async softElementAssertion(mobileElement) {
+        try {
+            await expect(mobileElement).toBeVisible();
+            return true;
+        } catch {
+            return false;
+        }
     }
 }
 
